377726fa4d97ec0b739fc1b6838860361c68173a
ophir <pere.jobs@gmail.com>
2018-10-07 21:16:14 +0200

    add new benchmark for nested objects

--- benches/json_encoding.rs
+++ benches/json_encoding.rs
@@ -6,7 +6,7 @@ extern crate serde_json;
 use criterion::{Criterion, Fun};
 use json_in_type::*;
 
-fn encode_json_in_type(n: f64) -> Vec<u8> {
+fn simple_json_in_type(n: f64) -> Vec<u8> {
     let obj = json_object! {
         void: (),
         list: json_list![1,2,3,n],
@@ -15,7 +15,7 @@ fn encode_json_in_type(n: f64) -> Vec<u8> {
     obj.to_json_buffer()
 }
 
-fn encode_serde(n: f64) -> Vec<u8> {
+fn simple_serde(n: f64) -> Vec<u8> {
     let obj = serde_json::json!({
         "void": null,
         "list": [1,2,3,n],
@@ -24,14 +24,71 @@ fn encode_serde(n: f64) -> Vec<u8> {
     serde_json::to_vec(&obj).unwrap()
 }
 
+
+fn nested_json_in_type(n: u8) -> Vec<u8> {
+    // 8 levels of nested objects
+    let obj = json_object! {
+        nested: json_object! {
+            nested: json_object! {
+                nested: json_object! {
+                    nested: json_object! {
+                        nested: json_object! {
+                            nested: json_object! {
+                                nested: json_object! {
+                                    nested: json_object! {
+                                        value: n
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    };
+    obj.to_json_buffer()
+}
+
+fn nested_serde(n: u8) -> Vec<u8> {
+    let obj = serde_json::json
+    !({
+        "nested": {
+            "nested": {
+                "nested": {
+                    "nested": {
+                        "nested": {
+                            "nested": {
+                                "nested": {
+                                    "nested": {
+                                        "value": n
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    });
+    serde_json::to_vec(&obj).unwrap()
+}
+
 fn criterion_benchmark(c: &mut Criterion) {
     c.bench_functions(
         "encode simple object",
         vec![
-            Fun::new("json_in_type", |b, i| b.iter(|| encode_json_in_type(*i))),
-            Fun::new("serde_json", |b, i| b.iter(|| encode_serde(*i))),
+            Fun::new("json_in_type", |b, i| b.iter(|| simple_json_in_type(*i))),
+            Fun::new("serde_json", |b, i| b.iter(|| simple_serde(*i))),
         ],
         999_999_999.999f64,
+    );
+    c.bench_functions(
+        "encode nested objects",
+        vec![
+            Fun::new("json_in_type", |b, i| b.iter(|| nested_json_in_type(*i))),
+            Fun::new("serde_json", |b, i| b.iter(|| nested_serde(*i))),
+        ],
+        0,
     );
 }
 

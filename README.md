9320610b3c0e60ef4991b893edc9d87c4ae9f39c
ophir <pere.jobs@gmail.com>
2018-10-07 18:43:52 +0200

    Add example to readme

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # json-in-types
 Fast json encoder in rust, that encodes the structure of JSON values in their types 
+
+## Exemple use
+
+```rust
+extern crate json_in_type;
+
+use json_in_type::*;
+
+fn main() {
+    let json_val = JSON(json_object!{
+        void: (),
+        list: json_list![1,2,3],
+        hello: "world"
+    });
+    println!("{}", json_val);
+}
+```
--- src/bin.rs
+++ src/bin.rs
@@ -3,29 +3,10 @@ extern crate json_in_type;
 use json_in_type::*;
 
 fn main() {
-    let v = json_list![
-        true,
-        '\n',
-        json_object!{
-            x: 1,
-            hello: String::from("world")
-        },
-        vec![Some("val"), None],
-        (),
-        Box::new("I ❤️ \"pépé\" \n backslash: \\!!!"),
-        Box::new(
-            JSONListElem::new(
-                1, JSONListElem::new(
-                    "lala",
-                    JSONListEnd,
-                ),
-            )
-        ),
-        Box::new(JSONListEnd),
-        Box::new(vec![
-            ("a", 1),
-            ("b", 1)
-        ])
-    ];
-    println!("{}", JSON(v));
+    let json_val = JSON(json_object!{
+        void: (),
+        list: json_list![1,2,3],
+        hello: "world"
+    });
+    println!("{}", json_val);
 }

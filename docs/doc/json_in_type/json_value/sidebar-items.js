acd51e0204b17e528e3023a8c7e6d7fdd0a67e78
ophir <pere.jobs@gmail.com>
2018-10-23 23:20:35 +0200

    Update docs and benches

--- docs/doc/json_in_type/json_value/sidebar-items.js
+++
@@ -1,1 +0,0 @@
-initSidebarItems({"struct":[["JSON","Encapsulates a JSONValue and implements useful traits."]],"trait":[["JSONValue","A trait implemented by types that can be serialized to JSON"]]});
